7d0d75235287d767cec1f62e657f11a4f8f600be
Brigant <sergeyjuly@gmail.com>
2022-06-03 17:53:05 +0300

    dev2 add new func 'Calculate()'

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+func Calculete(i, j int) int {
+	return i + j
+}
+
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("Here dev1 add new func and make some fix \"ShowMsg(msg string) error\"")
+
+	fmt.Println(Calculete(5, 7))
 
 }
