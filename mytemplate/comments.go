11f42a82dd592e15238912d00009caf22d8efd3b
Brigant <sergeyjuly@gmail.com>
2022-06-03 19:04:48 +0300

    dev3 add func CreateComment

--- mytemplate/comments.go
+++ mytemplate/comments.go
@@ -2,6 +2,11 @@ package mytemplate
 
 import "fmt"
 
+type Comment struct {
+	Title string
+	Text  string
+}
+
 func ShowComments() {
 	fmt.Println("Comment 1")
 	fmt.Println("Comment 2")
@@ -9,4 +14,8 @@ func ShowComments() {
 	fmt.Println("Comment 4")
 	fmt.Println("Comment 5")
 	fmt.Println("Comment 6")
+}
+
+func CreateComment(title, text string) *Comment {
+	return &Comment{Title: title, Text: text}
 }
