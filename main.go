1bc328e16a2a5ffd1e822a689ab94efc361dfb8f
Brigant <sergeyjuly@gmail.com>
2022-06-03 17:56:38 +0300

    dev2 add some fix with 'Calculate()'

--- main.go
+++ main.go
@@ -2,10 +2,6 @@ package main
 
 import "fmt"
 
-func Calculete(i, j int) int {
-	return i + j
-}
-
 func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("Here dev1 add new func and make some fix \"ShowMsg(msg string) error\"")
